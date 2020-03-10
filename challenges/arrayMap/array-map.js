30ce7fa3b143295d6ed99a3148627d5e053c1604
Rex Bergeron <rexbergeron@comcast.net>
2020-03-09 17:52:48 -0700

    same as old branch just no node modules

--- challenges/arrayMap/array-map.js
+++ challenges/arrayMap/array-map.js
@@ -1,6 +1,8 @@
 const map = (arr, func) => {
     const result = []
+    //loop through array
     for(let i = 0; i < arr.length; i++){
+        //apply the function to each item in array
         result[i] = func(arr[i])
     }
     return result;
