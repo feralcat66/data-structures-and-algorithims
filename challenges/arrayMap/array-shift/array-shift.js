1cabe80f0ae8b3297967aff55743abd17b7e033a
Rex Bergeron <rexbergeron@comcast.net>
2020-03-11 08:32:43 -0700

    I tried

---
+++ challenges/arrayMap/array-shift/array-shift.js
@@ -0,0 +1,12 @@
+const insertShiftArray = (arr, num) => {
+   const newResult = newResult.push(arr, ...num);
+        return newResult;   
+};
+
+
+const number = 5;
+
+module.exports = {
+    insertShiftArray,
+    number
+}
